1643f875ab855e96f466df9b5c4cb67678d845a1
jaronlee <jaronjlee@gmail.com>
2020-08-17 15:11:59 -0700

    added setup files

---
+++ dischord/frontend/actions/components/reducers/store/util/dischord.jsx
@@ -0,0 +1,11 @@
+//React
+import React from "react";
+import ReactDOM from "react-dom";
+// Components
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root");
+
+    ReactDOM.render(<h1>Welcome to Dischord</h1>, root);
+});
